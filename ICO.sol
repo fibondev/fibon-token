1cc1c9a0188d5a14ad6f4068d4ea0fd5ea95befa
Batuhan Özcan <batuhan.ozcan97@gmail.com>
2024-09-04 06:19:45 +0300

    Update ICO.sol

--- ICO.sol
+++ ICO.sol
@@ -4,8 +4,11 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract FibonICO is Ownable, ReentrancyGuard {
+    using SafeERC20 for IERC20;
+
     IERC20 public token;
     uint256 public rate;
     uint256 public startTime;
